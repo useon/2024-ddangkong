f3108c08258a7ee7f4927a7aa1b2a99d5d4e8feb
novice0840 <magnolia0840@gmail.com>
2024-07-25 22:32:06 +0900

    feat: API 주소 상수화 #74

--- frontend/src/constants/url.ts
+++ frontend/src/constants/url.ts
@@ -4,6 +4,13 @@ export const API_URL = {
   balanceContent: (roomId: number) => `${BASE_URL}/api/balances/rooms/${roomId}/content`,
   vote: (roomId: number, contentId: number) =>
     `${BASE_URL}/api/balances/rooms/${roomId}/contents/${contentId}/votes`,
+  makeRoom: `${BASE_URL}/api/balances/rooms`,
+  roomMember: (roomId: number) => `${BASE_URL}/api/balances/rooms/${roomId}/members`,
+  voteFinished: (roomId: number, contendId: number) =>
+    `${BASE_URL}/api/balances/rooms/${roomId}/contents/${contendId}/vote-finished`,
+  voteResult: (roomId: number, contendId: number) =>
+    `${BASE_URL}/api/balances/rooms/${roomId}/contents/${contendId}/vote-result`,
+  nextRound: (roomId: number) => `/api/balances/rooms/${roomId}/contents`,
 };
 
 export const MOCK_API_URL = {
