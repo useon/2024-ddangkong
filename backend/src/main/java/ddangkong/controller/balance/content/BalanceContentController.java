14fdf97900c047f020c7db5c0e94226a72e2b1dc
Nam Gi Beom <rlqja0523@naver.com>
2024-08-08 19:48:19 +0900

    test: response body 로그

--- backend/src/main/java/ddangkong/controller/balance/content/BalanceContentController.java
+++ backend/src/main/java/ddangkong/controller/balance/content/BalanceContentController.java
@@ -4,6 +4,7 @@ import ddangkong.controller.balance.content.dto.BalanceContentResponse;
 import ddangkong.service.balance.content.BalanceContentService;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,12 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 @Validated
 @RequiredArgsConstructor
+@Slf4j
 public class BalanceContentController {
 
     private final BalanceContentService balanceContentService;
 
     @GetMapping("/balances/rooms/{roomId}/content")
     public BalanceContentResponse getBalanceContent(@PathVariable @Positive Long roomId) {
-        return balanceContentService.getRecentBalanceContent(roomId);
+        BalanceContentResponse recentBalanceContent = balanceContentService.getRecentBalanceContent(roomId);
+        log.info("recentBalanceContent: {}", recentBalanceContent);
+        return recentBalanceContent;
     }
 }
